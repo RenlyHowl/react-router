109b16fda6bc6fb8de5ae34d08a20c464092b245
renly <1183957700@qq.com>
2020-01-15 10:06:14 +0800

    react-router的应用

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Home,
+  Article,
+  Users,
+  ArticleDetail,
+  NotFound
+} from "./views"
+import {Route, NavLink as Link, Redirect, Switch} from "react-router-dom"
+
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false
+    }
+  }
+  render() {
+    // console.log(this.props)
+    return(
+      <>
+      <ul>
+        <li><Link to="/home" activeStyle={{color: "red"}}>首页</Link></li>
+        <li><Link to="/article">文章</Link></li>
+        <li><Link to="/users">用户</Link></li>
+      </ul>
+      {/* Switch包裹 匹配渲染一个组件 */}
+      <Switch>
+
+      {/* 关于Route组件有几种写法 */}
+      <Route path="/home" component={Home} ></Route>
+      <Route path="/article" exact><Article></Article></Route>
+
+      {/* <Route path="/users" component={Users} /> */}
+      {/* 使用render方法做权限认证 */}
+      <Route path="/users" render={(props) => {
+        return this.state.isLoading ? <Users {...props}></Users> : "请登录"
+      }} />
+
+      <Route path="/article/:id" component={ArticleDetail} /> 
+      {/* <Route path="/404" component={NotFound}></Route> */}
+      {/* <Route path="/404"><NotFound tittle="component另一种传的参数"></NotFound> </Route> */}
+      <Route path="/404" render={(props) => {
+        // console.log(props)
+        return (
+          <>
+          {/* 将上面的props属性解构传过去 */}
+          <NotFound {...props} tittle="render传的参数"></NotFound>
+          </>
+        )
+      }}></Route>
+      <Redirect to="/home" from="/" exact/>
+      {/* 下面是上面所有的都没匹配到 全部重定位到404;但是注意上面会优先重定位到;故需要加exact全部匹配 */}
+      <Redirect to="/404" />
+      </Switch>
+      </>
+    )
+  }
+
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDom from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App.js"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {BrowserRouter as Router, Route} from "react-router-dom"
+/*
+要使用react-router的时候,我们需要在最外层包裹BrowserRouter;
+所以我们在App根组件外面包裹
+*/
+ReactDom.render(
+  <Router>
+    <Route component={App}></Route>
+  </Router>,
+  document.querySelector("#root")
+)
